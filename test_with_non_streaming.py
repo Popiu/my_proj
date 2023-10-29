25a1468eece67af5f1407dcf87fdfe0cd1f79248
lijia <>
2023-10-29 08:56:42 +0000

    Test successful with librosa

--- test_with_non_streaming.py
+++ test_with_non_streaming.py
@@ -1,17 +1,18 @@
 from pathlib import Path
+import librosa
 import utils.argument_par as argument_par
 import utils.recognizer_utils as recognizer_utils
 import utils.wave_utils as wave_utils
-# Make sure that the wave file is a single channel 16-bit encoded.
+
 if __name__ == "__main__":
     print("Hello")
     args = argument_par.get_args()
     args.tokens="./sherpa-onnx-paraformer-zh-2023-03-28/tokens.txt"
-    args.paraformer="./sherpa-onnx-paraformer-zh-2023-03-28/model.onnx"
+    args.paraformer="./sherpa-onnx-paraformer-zh-2023-03-28/model.int8.onnx"
     print(args)
     recognizer = recognizer_utils.create_recognizer(args)
     input_stream = recognizer.create_stream()
-    samples, sample_rate = wave_utils.read_wave("./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/0.wav")
+    samples, sample_rate = librosa.load("./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/my_recording.wav", sr=None, mono=True)
     input_stream.accept_waveform(sample_rate, samples)
     res = recognizer.decode_stream(input_stream)
 
--- utils/wave_utils.py
+++ utils/wave_utils.py
@@ -2,13 +2,6 @@ import wave
 import numpy as np
 
 def read_wave(wave_filename):
-    # wr = wave.open(file_path)
-    # num_frame = wr.getnframes()
-    # all_frames = wr.readframes(num_frame)
-    # array = np.frombuffer(all_frames, dtype=np.uint8)
-    # # normalize this array to
-    # sample_rate = wr.getframerate()
-    # return sample_rate, array
     with wave.open(wave_filename) as f:
         assert f.getnchannels() == 1, f.getnchannels()
         assert f.getsampwidth() == 2, f.getsampwidth()
