b242c2e0a1537906c7c2b0e8daf3281b0d259055
pgg3 <carljackson888777@gmail.com>
2023-12-24 19:33:55 +0800

    Update

---
+++ routing/start/init_send_message.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, node_id):
+        self.rreq_count = 0
+
+    def send_packet(self, packet, dest_addr):
+        # send packet
+        pass
+
+    def send_rreq(self):
+        # send RREQ packet
+        self.rreq_count += 1
+        rreq_id = self.rreq_count
+        src_addr = self.node_id
+        dest_addr = 'n10'
+        hop_count = 0
+        rreq = 'RREQ %d %s %s %d' % (rreq_id, src_addr, dest_addr, hop_count)
+        self.send_packet(rreq, dest_addr)
+
+    def send_rrep(self):
+        # send RREP packet
+        pass
+
+    def send_rerr(self):
+        # send RERR packet
+        pass
+
+if __name__ == '__main__':
+    node = Node('n1')
