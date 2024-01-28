cda6c5ac02a3fd24d531b49e6b9316a5a2830218
pgg3 <carljackson888777@gmail.com>
2024-01-28 11:58:15 +0800

    Start testing

--- routing/aodv_sim/Tester.py
+++ routing/aodv_sim/Tester.py
@@ -1,25 +1,28 @@
-class tester:
-    def __init__(self):
-        self.num_nodes = 0
-        self.sock = 0
-        self.port = 0
-        self.command = ""
+class Tester:
+    def __init__(self, test_config):
+        self.test_config = test_config
+        self.num_nodes = test_config["num_nodes"]
+        self.input_hint = (
+            "Enter a command:\n"
+            "1. H or h: Help\n"
+            "2. send <node_id_A> <node_id_B> <message> : Send message from A to B.\n")
 
-    def main(self, n):
-        # Store the node count
-        self.num_nodes = n
-
-        # # Setup the port to be used for communication with data nodes
-        # self.port = TESTER_PORT
-        #
-        # # Setup socket to communicate with the AODV protocol handler thread
-        # self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # self.sock.bind(('localhost', self.port))
-        # self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def main(self):
 
         # Set the prompt
         prompt = "TESTER" + "> "
 
         # Listen indefinitely for user inputs
+        print(self.input_hint)
         while (True):
             command = input(prompt)
+            command = command.split(" ")
+            if command[0] == "H" or command[0] == "h":
+                print(self.input_hint)
+            elif command[0] == "send":
+                node_id_A = int(command[1])
+                node_id_B = int(command[2])
+                message = command[3]
+                # self.send_message(node_id_A, node_id_B, message)
+            else:
+                print("Invalid command.")
